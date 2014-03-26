ac6793afc1df75b46d39e60c2f55952c08b3b856
Erik Kerber <erik.kerber@target.com>
2014-03-25 19:41:42 -0500

    Re-add XCTest version of Kiwi.

--- Src/Lib/BuddySDK Unit Tests/BuddyTestData.h
+++ Src/Lib/BuddySDK Unit Tests/BuddyTestData.h
@@ -1,16 +1,8 @@
 
 
-//#define APP_NAME @"bbbbbc.dbbbvsCLjDlj"
-//#define APP_KEY @"9B0BD1FA-11EC-454B-9544-7E8532E08806"
+#pragma message ("Insert your own AppID and Key")
+#define APP_NAME @"YOUR_APP_ID"
+#define APP_KEY @"YOUR_APP_KEY"
 
-// api.buddyplatform.com
-//#define APP_NAME @"bbbbbc.LffbvrjmtjHg"
-//#define APP_KEY @"5CD4E19E-BFE9-4185-B8AD-88845F3C4282"
-
-// api.buddyplatform.com
-#define APP_NAME @"bbbbbc.xkdbbdKlvKgj"
-#define APP_KEY @"EFE64026-B01A-4475-A75F-9F86B040094F"
-
-
-#define TEST_USERNAME @"erik3"
+#define TEST_USERNAME @"testuser"
 #define TEST_PASSWORD @"password"
